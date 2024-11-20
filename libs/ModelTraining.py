808b3e81ec67698adc0e7eb02876429417391efa
Xinxiang <angelowxx@gmail.com>
2024-11-20 23:44:10 +0100

    finish ModelTraining.py

--- libs/ModelTraining.py
+++ libs/ModelTraining.py
@@ -1,68 +1,93 @@
 import torch.nn as nn
-from libs.Network import TransformerMultiFeature
+from torch import optim
+from torch.utils.data import DataLoader
+
+from libs.Network import MultiLayersModel
 import torch
-from libs.DataPreparation import DataPreparation
+from libs.DataPreparation import DataPreparation, Dataset
 import matplotlib.pyplot as plt
+from libs.Network import MultiLayersModel
 
 
-symbol_list = ["sh000001", "sh603259"]
-data_preparation = DataPreparation()
-data, X, Y, stds, means = data_preparation.processCsv(symbol_list)
+def train(symbol_list, past_nums, prict_nums, num_epochs = 10):
+
+
+    data_preparation = DataPreparation()
+    X, Y, means, stds, colums = data_preparation.processCsv(symbol_list, past_nums, prict_nums)
+    X_train, X_test = torch.split(X, int(X.shape[0] * 0.9), dim=0)
+    Y_train, Y_test = torch.split(Y, int(Y.shape[0] * 0.9), dim=0)
+
+    model = MultiLayersModel(X_train.shape[1]*X_train.shape[2], 2, 4, Y_train.shape[1]*Y_train.shape[2])
+
+    loss_fn = nn.MSELoss()
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.5)
+
+    dataset = Dataset(X_train, Y_train)
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+
+    for epoch in range(num_epochs):
+        for batch_idx, (inputs, targets) in enumerate(dataloader):
+            # Forward pass
+            inputs = inputs.flatten(start_dim=1, end_dim=-1)
+            targets = targets.flatten(start_dim=1, end_dim=-1)
+            outputs = model(inputs)
+            loss = loss_fn(outputs, targets)
+
+            # Backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        scheduler.step()
+
+        print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}")
+
+    valuate_model(model, X_test, Y_test, loss_fn, colums)
+    return model
+
+def valuate_model(model, X_test, Y_test, loss_fn, colums):
+    Y_pred = model(X_test.flatten(start_dim=1, end_dim=-1))
+    Y_pred = Y_pred.view(Y_test.shape[0], Y_test.shape[1], Y_test.shape[2])
+    eval_loss = loss_fn(Y_pred, Y_test)
+
+    Y_pred = Y_pred.detach().numpy()
+    Y_test = Y_test.detach().numpy()
+
+    # close_market, volume_market, close_stock, volume_stock, outstanding_share_stock, turnover
+    l = len(colums)
+    width = 2
+    height = (int)(l / width)
+    if l % width != 0:
+        height += 1
+
+    fig, axis = plt.subplots(height, width, figsize=(10, 8))
+    for i in range(height):
+        for j in range(width):
+            num = i * width + j
+            if num >= l:
+                break
+            axis[i, j].plot(Y_test[0, :, num], color="red", linestyle="--", label="test data")
+            axis[i, j].plot(Y_pred[0, :, num], color="blue", linestyle="--", label="pred data")
+            axis[i, j].set_title(colums[num])
+            axis[i, j].set_xlabel("predicted day")
+            axis[i, j].set_ylabel("value")
+            axis[i, j].legend()
+
+    plt.tight_layout()
+    fig.suptitle(f"The total evaluation loss: {eval_loss.item()}", fontsize=13)
+    plt.show()
+
+
 
 # Initialize model
-num_features = data.shape[1]  # Number of features in the dataset
-model = TransformerMultiFeature(num_features=num_features)
+if __name__ == '__main__':
 
-criterion = nn.MSELoss()  # Use MSE for regression tasks
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-# Training loop
-num_epochs = 100
-batch_size = 32
-
-for epoch in range(num_epochs):
-    model.train()
-    epoch_loss = 0
-    for i in range(0, len(X), batch_size):
-        X_batch = X[i : i + batch_size]
-        Y_batch = Y[i : i + batch_size]
-
-        # Prepare target input and output for the decoder
-        tgt = Y_batch[:, :-1, :]  # Remove the last step
-        tgt_output = Y_batch[:, 1:, :]  # Remove the first step
-
-        optimizer.zero_grad()
-        output = model(X_batch, tgt)
-        loss = criterion(output, tgt_output)
-        loss.backward()
-        optimizer.step()
-
-        epoch_loss += loss.item()
-    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}")
+    symbol_list = ["sh000001", "sh603259"]
+    past_nums = 30
+    prict_nums = 10
 
 
-input_window = 50
-output_window = 5
-model.eval()
-with torch.no_grad():
-    # Provide an input sequence
-    input_seq = torch.tensor(data[-input_window-output_window:len(data)-output_window, :], dtype=torch.float32).unsqueeze(0)  # Shape: (1, input_window, num_features)
 
-    # Initialize target sequence with zeros
-    tgt_seq = torch.zeros((1, output_window, num_features), dtype=torch.float32)  # Shape: (1, output_window-1, num_features)
-
-    # Predict future sequence
-    output = model(input_seq, tgt_seq).squeeze().numpy()
-
-output = output * stds + means
-data = data * stds + means
-
-plt.plot(output[:, 2], color="red", linestyle="--")
-plt.plot(data[-output_window:, 2], color="blue", linestyle="--")
-# Add labels and title
-plt.title("Sequence Plot")
-plt.xlabel("Index")
-plt.ylabel("Value")
-plt.show()
-print("Predicted sequence shape:", output.shape)
-
+    train(symbol_list, past_nums, prict_nums, 100)
